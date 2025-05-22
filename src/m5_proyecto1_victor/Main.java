e13a381316872a7519a7d998d7bf46109cb6a3d5
adam23vcatala <adam23vcatala@inslaferreria.cat>
2025-05-22 18:29:41 +0200

    Segundo commit

--- src/m5_proyecto1_victor/Main.java
+++ src/m5_proyecto1_victor/Main.java
@@ -2,9 +2,13 @@ package m5_proyecto1_victor;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hola");
-        System.out.println("Adiós");
+    public static void main(String[] args) throws InterruptedException {
+        String msg = "¿Hola, que tal?";
+        
+        for (int i = 0; i <= msg.length(); i++) {
+            System.out.println(msg.substring(0, i));
+            Thread.sleep(800);
+        }
     }
     
 }
