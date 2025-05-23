f5c88a5ee453311f280d4bcf9b4e2f5cd7c6a4c9
Castar2107 <adam23icastarnado@inslaferreria.cat>
2025-05-23 16:58:52 +0200

    hola

--- src/m5_proyecto1_victor/Main.java
+++ src/m5_proyecto1_victor/Main.java
@@ -3,7 +3,11 @@ package m5_proyecto1_victor;
 public class Main {
 
     public static void main(String[] args) {
+        a= 0;
         System.out.println("Hola");
+        if (a = 2) {
+            
+        }
         System.out.println("Adiós");
     }
     
