956ca1cc50dbdd0b1195208af3eacf6bec7723b4
adam23vcatala <adam23vcatala@inslaferreria.cat>
2025-05-26 19:46:21 +0200

    Cuarto commit

--- src/m5_proyecto1_victor/Main.java
+++ src/m5_proyecto1_victor/Main.java
@@ -4,13 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
         EntradaSalidaDatos esd = new EntradaSalidaDatos();
-        esd.mostrarCadena("Introduce un número: ");
+        esd.mostrarCadena("Introduce primer número: ");
         int a = esd.introducirValorEnteroPositivo();
+        esd.mostrarCadena("Introduce segundo número: ");
+        int b = esd.introducirValorEnteroPositivo();
+        esd.mostrarCadena("Introduce tercer número: ");
+        int c = esd.introducirValorEnteroPositivo();
         
-        if (a % 2 == 0) {
-            esd.mostrarCadena(a + " es un número par");
+        if (a > b && b > c) {
+            esd.mostrarCadena(c + " es mayor que " + a);
+            if (a < b) {
+                esd.mostrarCadena(a + " es menor que " + b);
+            } else {
+                esd.mostrarCadena(a + " es mayor que " + b);
+            }
         } else {
-            esd.mostrarCadena(a + " es un número impar");
+            esd.mostrarCadena(c + " es menor que " + a);
+            if (b < c) {
+                esd.mostrarCadena(b + " es menor que " + c);
+            } else {
+                esd.mostrarCadena(b + " es mayor que " + c);
+            }
         }
     }
 
