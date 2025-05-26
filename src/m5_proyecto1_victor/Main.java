c1fa9a86dfe3a7f92e9e6ceb4b00cef9a3787255
adam23vcatala <adam23vcatala@inslaferreria.cat>
2025-05-26 19:58:39 +0200

    Juegos agregados

--- src/m5_proyecto1_victor/Main.java
+++ src/m5_proyecto1_victor/Main.java
@@ -3,29 +3,8 @@ package m5_proyecto1_victor;
 public class Main {
 
     public static void main(String[] args) {
-        EntradaSalidaDatos esd = new EntradaSalidaDatos();
-        esd.mostrarCadena("Introduce primer número: ");
-        int a = esd.introducirValorEnteroPositivo();
-        esd.mostrarCadena("Introduce segundo número: ");
-        int b = esd.introducirValorEnteroPositivo();
-        esd.mostrarCadena("Introduce tercer número: ");
-        int c = esd.introducirValorEnteroPositivo();
-        
-        if (a > b && b > c) {
-            esd.mostrarCadena(c + " es mayor que " + a);
-            if (a < b) {
-                esd.mostrarCadena(a + " es menor que " + b);
-            } else {
-                esd.mostrarCadena(a + " es mayor que " + b);
-            }
-        } else {
-            esd.mostrarCadena(c + " es menor que " + a);
-            if (b < c) {
-                esd.mostrarCadena(b + " es menor que " + c);
-            } else {
-                esd.mostrarCadena(b + " es mayor que " + c);
-            }
-        }
+        Juegos j = new Juegos();
+        j.menu();
     }
 
 }
